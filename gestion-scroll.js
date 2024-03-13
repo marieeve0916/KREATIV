f58f4abc478c416fab76766a99813bd55f7e048d
marieeve0916 <112022065+marieeve0916@users.noreply.github.com>
2024-03-13 16:40:49 -0400

    Avancements JavaScript

---
+++ gestion-scroll.js
@@ -0,0 +1,27 @@
+// Empêcher le défilement sur la page lorsque le menu burger est ouvert
+let ScheckBox = document.querySelector("#case-bouton-burger");
+let Sbody = document.querySelector("body");
+
+ScheckBox.addEventListener("click", gererDefilement);
+
+function gererDefilement() {
+    if (ScheckBox.checked == true) {
+        Sbody.style.overflow = "hidden";
+        console.log(Sbody.style.overflow)
+    } else {
+        Sbody.style.overflow = "scroll";
+    }
+}
+
+let boutons = document.querySelectorAll(".menu a");
+
+for (let unBouton of boutons) {
+    unBouton.addEventListener("click", controlerAffichageMenu);
+}
+
+function controlerAffichageMenu() {
+    if (ScheckBox.checked == true) {
+        ScheckBox.checked = false;
+        Sbody.style.overflow = "scroll";
+    }
+}
